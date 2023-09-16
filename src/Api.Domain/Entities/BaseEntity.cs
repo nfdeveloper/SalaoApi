dcfd93ef2c9f0f895af2676ac786f024b9f9137b
nfdeveloper <nfdeveloper04@gmail.com>
2023-09-15 23:19:34 -0300

    Criando Entidade CategoryService e instalando pacotes necessários para Entity Framework

--- src/Api.Domain/Entities/BaseEntity.cs
+++ src/Api.Domain/Entities/BaseEntity.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,17 @@ namespace Api.Domain.Entities
 {
     public class BaseEntity
     {
+        [Key]
         public int Id { get;set; }
+        [Column(name:"status")]
         public String Status { get;set; }
+        [Column(name:"created_at")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy hh:mm}", ApplyFormatInEditMode = true)]
         public DateTime CreatedAt { get;set; } = DateTime.Now;
+        [Column(name:"updated_at")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy hh:mm}", ApplyFormatInEditMode = true)]
         public DateTime UpdatedAt { get;set; } = DateTime.Now;
         public String UsuarioCriacao { get;set; }
         public String UsuarioAlteracao { get;set; }
